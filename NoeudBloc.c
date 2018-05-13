35fc76494e2e6e7a368606a19a6f1cefd7d77f2f
Pauline Capesius <pauline.capesius@outlook.fr>
2018-05-13 15:16:20 +0200

    initialisation blocchain

--- NoeudBloc.c
+++ NoeudBloc.c
@@ -1,6 +1,7 @@
 #include "include.h"
 
 u_long prognum;
+bloc chainbloc[100] ;
 
 short inscription(int b) {
   static int res = 1;
@@ -19,6 +20,14 @@ int main (int argc, char *argv[]) {
   prognum = strtoul(argv[1], &entrop, 16) ;
   printf("prognum %lu\n", prognum);
   fflush(stdout);
+
+  operation operationvide = {"",0,0,0};
+
+  //chainbloc = malloc(sizeof(bloc)*100);
+  for(int i=0; i<100; i++) {
+    bloc bloccourant = {.operations ={operationvide,operationvide,operationvide,operationvide},.hash="3",.previoushash=""};
+    chainbloc[i] = bloccourant;
+  }
 
 
   registerrpc(prognum , VERSNUM, 1, inscription, (xdrproc_t) xdr_int, (xdrproc_t) xdr_int);
